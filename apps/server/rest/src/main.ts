db5ef78a9fc77eee51c4b8554204469884954e37
99mini <itman390@gmail.com>
2025-02-05 12:08:14 +0900

    chore: replace webpack to esbuild

--- apps/server/rest/src/main.ts
+++ apps/server/rest/src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-if (process.env.NODE_ENV === 'production') {
-  async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
-  }
-  bootstrap();
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
-
-export const viteNodeApp = NestFactory.create(AppModule);
+bootstrap();
