0fefc7da64d54a3c4001be6c6cf8e22986cbf043
99mini <itman390@gmail.com>
2025-02-10 16:36:55 +0900

    feat: api call

--- apps/frontend/tool/src/apis/health/health.ts
+++ apps/frontend/tool/src/apis/health/health.ts
@@ -1,14 +1,19 @@
+import { FetchResponse } from '../_modal';
 import { Health } from './heath.modal';
 
 export async function healthCheck(type: 'rest' | 'webhooks' = 'rest'): Promise<Health | null> {
   const endpoint = type === 'rest' ? 'health' : `webhooks/health`;
-
   try {
     const response = await fetch(`https://api.zerovoyage.com/v1/${endpoint}`);
 
     if (response.ok) {
-      const data = await response.json();
-      return data;
+      const data: FetchResponse<Health> = await response.json();
+
+      if (data.status === 200) {
+        return data.data;
+      }
+
+      return null;
     }
     return null;
   } catch (error) {
