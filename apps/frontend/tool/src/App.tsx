99a8d1bfaeed50addbff00dc44fd3fa4f0ec21bf
99mini <itman390@gmail.com>
2025-02-13 21:14:34 +0900

    fix(apps/frontend/tool): remove api call

--- apps/frontend/tool/src/App.tsx
+++ apps/frontend/tool/src/App.tsx
@@ -1,23 +1,8 @@
-import { useEffect } from 'react';
 import RootRouter from './RootRouter';
 
 import './index.css';
-import { healthCheck } from './apis';
 
 const App = () => {
-  /** 첫 방문자 확인 */
-  useEffect(() => {
-    const isFirstVisit = Boolean(localStorage.getItem('firstVisit'));
-    if (!isFirstVisit) {
-      localStorage.setItem('firstVisit', 'true');
-
-      /** health check */
-      healthCheck('rest').then((res) => {
-        console.debug(res);
-      });
-    }
-  }, []);
-
   return <RootRouter />;
 };
 
