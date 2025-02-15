d127e7c424a5f35c0c8fed075dc2af9edbf0cb89
99mini <itman390@gmail.com>
2025-02-15 11:57:53 +0900

    chore: apply tailwind

--- packages/vds/rollup.config.cjs
+++ packages/vds/rollup.config.cjs
@@ -5,6 +5,7 @@ import typescript from '@rollup/plugin-typescript';
 
 import pkg from './package.json';
 
+/** @type { import('rollup').RollupOptions[] } */
 export default [
   {
     input: './src/index.ts',
