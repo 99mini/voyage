77fc0ee6e6806a58e28fc002a4d345d94fd55fcb
99mini <43674669+99mini@users.noreply.github.com>
2025-02-20 16:08:47 +0900

    feat(apps/frontend/home): 홈페이지 프로젝트 연결한다 (#28)
    * chore: setting; feat: footer
    
    * feat: projects sectoin init
    
    * chore: tsconfig
    
    * docs: Update README.MD #24, #25 (#26)
    
    docs: Update README.MD
    
    * chore: issue test (#27)
    
    closes: #24, #25
    
    * feat: 홈페이지 프로젝트 연결

---
+++ apps/frontend/home/app/components/hero/hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-3xl md:text-4xl text-center">Welcome to Zerovoyage</h1>
+    </div>
+  );
+}
