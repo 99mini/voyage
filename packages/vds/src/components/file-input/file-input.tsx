b4390b75f581415ca2bf800cfb4aaf442b92786e
99mini <itman390@gmail.com>
2025-02-26 12:39:48 +0900

    feat: file-input 추가하기 버튼

--- packages/vds/src/components/file-input/file-input.tsx
+++ packages/vds/src/components/file-input/file-input.tsx
@@ -1,10 +1,8 @@
 import { cn } from '@/lib/utils';
+import { PlusCircleIcon, X } from 'lucide-react';
 import { useId, useState } from 'react';
 import Input, { type InputProps } from '../input';
 import Button from '../button';
-import { PlusCircleIcon, X } from 'lucide-react';
-
-
 
 export type FileInputProps = {
   className?: string;
@@ -23,6 +21,7 @@ const FileInput = ({ className, placeholderClassName, children, ...props }: File
     }
 
     setFiles(Array.from(event.target.files));
+    console.log(files);
   };
 
   const handleAppendFile = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -41,13 +40,15 @@ const FileInput = ({ className, placeholderClassName, children, ...props }: File
     setFiles(files.filter((f) => f !== file));
   };
 
-  
-
   return (
     <label
       // className based on Shadcn Input className
       className={cn(
-        `flex min-h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm`,
+        `flex min-h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors md:text-sm cursor-pointer`,
+        `file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground`,
+        `focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring`,
+        `disabled:cursor-not-allowed disabled:opacity-50`,
+        `placeholder:text-muted-foreground`,
         className,
       )}
       onClick={() => setReason('append')}
@@ -60,22 +61,40 @@ const FileInput = ({ className, placeholderClassName, children, ...props }: File
                 key={`${file.name}-${file.lastModified}`}
                 className="flex flex-row justify-between items-center gap-2 w-full"
               >
-                <div className="flex gap-2 w-full rounded-md p-2 hover:bg-gray-100" onClick={(e) => {e.preventDefault();}}>
-                  <span className="text-sm text-gray-400">{index + 1}</span>
-                  <span className="text-sm text-gray-700">{file.name}</span>
+                <div
+                  className="flex gap-2 w-full rounded-md p-1 hover:bg-gray-100"
+                  onClick={(e) => {
+                    e.preventDefault();
+                  }}
+                >
+                  <span className="text-xs text-gray-400">{index + 1}</span>
+                  <span className="text-xs text-gray-700">{file.name}</span>
                 </div>
-                  <X className="w-8 h-8 aspect-square p-1 rounded-full cursor-pointer text-gray-500 hover:bg-gray-200 hover:text-gray-700" onClick={(e) => {
+                <X
+                  className="w-6 h-6 aspect-square p-1 rounded-full cursor-pointer text-gray-500 hover:bg-gray-200 hover:text-gray-700"
+                  onClick={(e) => {
                     e.preventDefault();
                     handleRemove(file);
-                  }}/>
+                  }}
+                />
               </div>
             ))}
-          <label className="flex items-center justify-center cursor-pointer" htmlFor={inputId} onClick={() => setReason('append')}>
-            <PlusCircleIcon className="w-8 h-8 aspect-square p-1 rounded-full cursor-pointer text-gray-500 hover:bg-gray-200 hover:text-gray-700"/>
-          </label>
+            {props.multiple && (
+              <Button
+                color="primary"
+                className="flex justify-center items-center gap-2 p-2 w-full rounded-md"
+                variant={'ghost'}
+                asChild
+              >
+                <label className="flex items-center justify-center cursor-pointer w-full" htmlFor={inputId}>
+                  <PlusCircleIcon className="w-6 h-6 aspect-square rounded-full cursor-pointer" />
+                  <span className="text-sm">{'파일 추가'}</span>
+                </label>
+              </Button>
+            )}
           </div>
         ) : (
-          (children ?? '파일 선택')
+          (children ?? <span className="text-xs text-gray-700">{'파일 선택'}</span>)
         )}
       </div>
       <Input
