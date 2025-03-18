66df6cdf86a0d376d411879d8d0e917036f075e6
99mini <itman390@gmail.com>
2025-03-18 19:40:29 +0900

    chore: 개발 환경에서 strict mode 사용하지 않음

--- apps/frontend/awesome/src/main.tsx
+++ apps/frontend/awesome/src/main.tsx
@@ -4,8 +4,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const mode = import.meta.env.NODE_ENV;
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  mode === 'development' ? (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  ) : (
     <App />
-  </StrictMode>,
+  ),
 );
