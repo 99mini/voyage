00e25a35f2bc7ea7430414fd976e28c842ab4b74
99mini <itman390@gmail.com>
2025-05-05 22:49:05 +0900

    feat: client only rendering

--- apps/frontend/about/src/components/hero/hero.tsx
+++ apps/frontend/about/src/components/hero/hero.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import ClientOnly from '../utils/client-only';
+
 const WakaTimeGraph = React.lazy(() => import('../indicator/waka-graph'));
 
 const Hero = () => {
   return (
     <div className="container mx-auto py-10 px-4">
       <h1 className="text-3xl md:text-4xl text-center">Welcome to Zerovoyage</h1>
-      <WakaTimeGraph />
+      <ClientOnly>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <WakaTimeGraph />
+        </React.Suspense>
+      </ClientOnly>
     </div>
   );
 };
