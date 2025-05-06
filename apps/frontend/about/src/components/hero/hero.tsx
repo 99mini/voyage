a85a2bdb0d143ca5a69850af315dd65bb4070393
99mini <itman390@gmail.com>
2025-05-06 21:05:32 +0900

    comment: 그래프 주석 처리

--- apps/frontend/about/src/components/hero/hero.tsx
+++ apps/frontend/about/src/components/hero/hero.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 import ClientOnly from '../utils/client-only';
 
+// TODO: 그래프 구현 완료하기
 const WakaTimeGraph = React.lazy(() => import('../indicator/waka-graph'));
 
 const Hero = () => {
   return (
     <div className="container mx-auto py-10 px-4">
       <h1 className="text-3xl md:text-4xl text-center">Welcome to Zerovoyage</h1>
-      <ClientOnly>
+      {/* <ClientOnly>
         <React.Suspense fallback={<div>Loading...</div>}>
           <WakaTimeGraph />
         </React.Suspense>
-      </ClientOnly>
+      </ClientOnly> */}
     </div>
   );
 };
